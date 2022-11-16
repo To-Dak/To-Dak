4d97bd7b21e28d418b89276c2d997e25f77b6bfc
yeeeeeeeejun <96407905+yejun0327@users.noreply.github.com>
2022-11-16 19:42:24 +0900

    Update main_submit.js
    텍스트박스 계속 텍스트로 둘건지 미팅 후 결정!

--- main_submit.js
+++ main_submit.js
@@ -1,9 +1,11 @@
 var textInput=document.getElementById("textbox");
 var submitButton=document.getElementById("submit");
 var toDoList=document.getElementById("list_health");
+var textbox=document.getElementsByClassName("textbox");
 
 //<+> 버튼 눌렀을때 addTextBox()
 submitButton.addEventListener('click',addTextBox);
+textbox.addEventListener('keypressed',EnterPressed);
 
 function addTextBox(){
   var divv=document.getElementById("healthList");
@@ -11,4 +13,11 @@ function addTextBox(){
   var textBox='<div class="singleList"><input type="checkbox" class="checks"><input type="text" class="textbox" placeholder="일정을 추가하세요."></input></div>'
   div.innerHTML=textBox;
   divv.appendChild(div);
+  
 }
+
+function EnterPressed(){
+  if (window.event.keyCode == 13){
+  }
+}
+// 추가할지 미팅 후 결정!
